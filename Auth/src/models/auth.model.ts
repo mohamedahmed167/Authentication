dc9f41938f47e62651af14a48a8ec7365290ecb5
mohamedahmed167 <marmo1672017@gmail.com>
2026-08-17 17:31:02 +0300

    create : User model

--- Auth/src/models/auth.model.ts
+++ Auth/src/models/auth.model.ts
@@ -1,15 +1,8 @@
-import express from "express"
-import dotenv from "dotenv"
-dotenv.config()
-const app = express()
-const PORT =process.env.PORT || 5000
-
-
-// middleWare
-app.use(express.json())
-
-
-
-app.listen(PORT,()=>{
-  console.log(`server is Running and your Port is ${PORT}`)
-})
+import mongoose from "mongoose";
+import { timeStamp } from "node:console";
+const UserSchema =new mongoose.Schema({
+  email:{type:String ,required:true,unique:true},
+  password:{type:String ,required:true},
+},{timestamps:true})
+const User =mongoose.model("User",UserSchema)
+export default User
