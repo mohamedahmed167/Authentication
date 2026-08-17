6ffb499249d26a0f2674ee8c26027b7389557ddf
mohamedahmed167 <marmo1672017@gmail.com>
2026-08-17 16:46:41 +0300

    create : connectDB function

---
+++ Auth/src/config/db.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+const connectDB =async()=>{
+  try{
+    await mongoose.connect(process.env.Mongo_URI || "")
+    console.log("MonogDB connected")
+  }catch(error){
+    console.log("MonogDB connetion error",error)
+    process.exit(1)    //عشان اوقف العمليه بعدها
+  }
+}
+export default connectDB
---
+++ Auth/src/models/auth.model.ts
@@ -0,0 +1,15 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+const app = express()
+const PORT =process.env.PORT || 5000
+
+
+// middleWare
+app.use(express.json())
+
+
+
+app.listen(PORT,()=>{
+  console.log(`server is Running and your Port is ${PORT}`)
+})
