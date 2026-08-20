89519a83950151adbe5548538a9d4b9712c5a878
mohamedahmed167 <marmo1672017@gmail.com>
2026-08-20 16:55:45 +0300

    add : Role in Usermodel

--- Auth/src/models/auth.model.ts
+++ Auth/src/models/auth.model.ts
@@ -1,7 +1,15 @@
 import mongoose from "mongoose";
-const UserSchema =new mongoose.Schema({
-  email:{type:String ,required:true,unique:true},
-  password:{type:String ,required:true},
-},{timestamps:true})
-const User =mongoose.model("User",UserSchema)
-export default User
+const UserSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role:{
+      type:String,
+      enum:["user","admin"],
+      default:"user"
+    }
+  },
+  { timestamps: true },
+);
+const User = mongoose.model("User", UserSchema);
+export default User;
